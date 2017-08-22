7e9fa994ef3620eb54d33b3cc9d19637438fac93
andy yee <andy.sksea@gmail.com>
2017-08-22 10:51:37 -0400

    add bcrypt and user controlle

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,2 +1,11 @@
 class Api::UsersController < ApplicationController
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      login!(@user)
+      render json: @user
+    else
+      render json: @user.errors.full_messages
+    end
+  end
 end
