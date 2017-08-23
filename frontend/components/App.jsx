3f8a0d04395ecd6644507ae3da8196b33b6c7e03
andy yee <andy.sksea@gmail.com>
2017-08-23 14:53:43 -0400

    start building main sections

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -5,19 +5,16 @@ import { AuthRoute, ProtectedRoute } from '../util/RouteUtil';
 import SignupModalContainer from './modals/SignupModalContainer';
 import LoginModalContainer from './modals/LoginModalContainer';
 import Main from './Main';
-// We need a header toolbar
+import Header from './Header';
 
 import { logout } from '../util/SessionApiUtil';
 
 const App = () => {
   return (
     <div id='app'>
-      <header>
-        <h1>Airbase</h1>
-      </header>
-      
       <Route exact path='/' component={SignupModalContainer} />
       <Route exact path='/' component={LoginModalContainer} />
+      <Route exact path='/' component={ Header } />
       <Route exact path='/' component={ Main } />
     </div>
   );
