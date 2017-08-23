d781476f9e2601a8db8bf73d248d44c26c9b74ea
andy yee <andy.sksea@gmail.com>
2017-08-23 17:32:52 -0400

    update navbar based on login status

--- frontend/components/Main.jsx
+++ frontend/components/Main.jsx
@@ -1,29 +1,11 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { logout } from '../actions/sessionActions';
-import { toggleLogin, toggleSignup } from '../actions/uiActions';
-
-// TODO: This can probable be changed into a functional component,
-// it's not maintaining any state, only rendering other elements.
-
-// TODO: Move logout button into nav.
-const mapDispatchToProps = (dispatch) => {
-  return {
-    logout: () => dispatch(logout()),
-    toggleLogin: () => dispatch(toggleLogin),
-    toggleSignup: () => dispatch(toggleSignup),
-  };
-};
 
 const Main = (props) => {    
   return (
     <section id='main'>
-      <h2>-------------DEBUG-------------</h2>
-      <div><button onClick={ props.logout }>Logout</button></div>
-      <div><button onClick={ props.toggleLogin }>Login</button></div>
-      <div><button onClick={ props.toggleSignup }>Signup</button></div>
+
     </section>
   );
 };
 
-export default connect(null, mapDispatchToProps)(Main);
+export default Main;
