ba3d280ff1c939b40e4e593a9f9940d8ea369efd
andy yee <andy.sksea@gmail.com>
2017-08-24 19:31:33 -0400

    style header

--- frontend/components/header/MainNav.jsx
+++ frontend/components/header/MainNav.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import MainNavMenu from './MainNavMenu';
+import Search from './Search';
 
 const MainNav = () => {
   return (
-    <div>
+    <div id='main-nav'>
+      <img id='abnb-logo' src={ window.staticImages.abnb } />
+      {/* <S */}
       <MainNavMenu />
     </div>
   );
