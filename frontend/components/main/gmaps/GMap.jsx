f80d12cb25f2813a552f9c0ae5afeb27bfd22612
andy yee <andy.sksea@gmail.com>
2017-08-25 17:33:09 -0400

    Add GMaps onto Home route

--- frontend/components/main/gmaps/GMap.jsx
+++ frontend/components/main/gmaps/GMap.jsx
@@ -4,23 +4,22 @@ class GMap extends React.Component {
   //...
 
   componentDidMount() {
-    // set the map to show SF
     const mapOptions = {
       center: { lat: 37.7758, lng: -122.435 }, // this is SF
       zoom: 13
     };
-
     // wrap the mapDOMNode in a Google Map
-    this.map = new window.google.maps.Map(this.mapNode, mapOptions);
+    this.map = new window.google.maps.Map(this.mapContainer, mapOptions);
   }
 
   render() {
     return (
-      // ...
-      <div>
-        <div id='map-container' ref={ map => this.mapNode = map }>
+      // https://facebook.github.io/react/docs/refs-and-the-dom.html#adding-a-ref-to-a-dom-element
+      // allows component to reference this div as this.mapContainer
+      <div id='map-wrapper'>
+        <div id='map-container' ref={ domEl => { this.mapContainer = domEl; } }>
         </div>
-      </div> 
+      </div>
     );
   }
 
--- frontend/components/main/homes/Homes.jsx
+++ frontend/components/main/homes/Homes.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import GMap from '../gmaps/GMap';
+import HomesList from './HomesList';
 
 const Homes = (props) => {
+  // Pass in map-container DOM element into React component as ref so
+  // it exists outside the component lifecycle.
+  // const mapContainer = document.getElementById('map-container');
   return (
-    <div>
-      <GMap />
+    <div id='homes-wrapper'>
+      <div id='homes'>
+        {/* Homes List */}
+        <GMap />
+      </div>
     </div>
   );
 };
