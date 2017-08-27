aab9f7b5815f308f541e08687d1b1cb528d078d0
andy yee <andy.sksea@gmail.com>
2017-08-27 02:05:26 -0400

    Update gmaps component style and center point

--- frontend/components/main/gmaps/GMap.jsx
+++ frontend/components/main/gmaps/GMap.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
+import GMapStyles from './GMapStyles';
 
 class GMap extends React.Component {
-  //...
 
   componentDidMount() {
+    const CENTER = {lat: 30.1301514, lng: -8.2019344};
     const mapOptions = {
-      center: {lat: -34.397, lng: 150.644},
-      zoom: 8,
+      center: CENTER,
+      zoom: 2,
+      styles: GMapStyles,
     };
     // wrap the mapDOMNode in a Google Map
     this.map = new google.maps.Map(this.mapContainer, mapOptions);
@@ -23,7 +25,6 @@ class GMap extends React.Component {
     );
   }
 
-  //...
 }
 
 export default GMap;
