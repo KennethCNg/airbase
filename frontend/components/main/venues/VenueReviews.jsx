1b88bf085432cde5ec64c0858f0ba942c03b9cb1
andy yee <andy.sksea@gmail.com>
2017-08-29 10:27:17 -0400

    add missing templates preventing build on heroku

---
+++ frontend/components/main/venues/VenueReviews.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class VenueReviews extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    
+  }
+  
+  componentDidMount() {
+    
+  }
+  
+  render() {
+    return (
+      <div id='venue-reviews'>
+        
+      </div>
+    );
+  }
+}
+
+export default VenueReviews;
---
+++ frontend/components/main/venues/VenueShowSecNav.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const VenueShowSecNav = props => {
+
+  return (
+    <div id='venue-show-section-nav'>
+      
+      
+    </div>
+  );
+};
+
+export default VenueShowSecNav;
