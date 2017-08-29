3984ca68a640dde55b7a772255f77ab078b57301
andy yee <andy.sksea@gmail.com>
2017-08-29 09:58:39 -0400

    More venue show styling

--- frontend/components/main/venues/VenueDescription.jsx
+++ frontend/components/main/venues/VenueDescription.jsx
@@ -11,27 +11,27 @@ const VenueDescription = ({ venue }) => {
         style={ style }>
       </div>
       <div className='ven-desc-body'>
-        <div className='ven-desc-title'>
-          <div>{ venue.name }</div>
+        <div className='ven-desc-sec ven-desc-title'>
+          <div className='ven-desc-title-name'>{ venue.name }</div>
           <div>
             { venue.city }, { venue.state }, { venue.country } 
             Reviews Placeholder
           </div>
         </div>
-        <div className='ven-desc-summary-icons'>
+        <div className='ven-desc-sec ven-desc-summary-icons'>
           <span>{ venue.room_type }</span>
           <span>{ venue.accommodates }</span>
           <span>{ venue.beds }</span>
           <span>{ venue.bedrooms }</span>
         </div>
-        <div className='ven-desc-about'>
+        <div className='ven-desc-sec ven-desc-about'>
           <span>{ venue.description_about }</span>
           <span>{ venue.description_space }</span>
           <span>{ venue.description_guest_access }</span>
           <span>{ venue.description_guest_interaction }</span>
           <span>{ venue.description_other_notes }</span>
         </div>
-        <div className='ven-desc-summary-table'>
+        <div className='ven-desc-sec ven-desc-summary-table'>
         </div>
         
         <div className=''></div>
