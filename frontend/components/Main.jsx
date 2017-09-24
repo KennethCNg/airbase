8e20344cf0b96aa52dc047b9e000d8568b7050b7
andy yee <andy.sksea@gmail.com>
2017-09-24 00:46:47 -0400

    Temporarily remove unused auth components

--- frontend/components/Main.jsx
+++ frontend/components/Main.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import { AuthRoute, ProtectedRoute } from '../util/RouteUtil';
-import Login from './auth/Login';
-import Signup from './auth/Signup';
 import VenuesContainer from './venues/VenuesContainer';
 import VenueShowContainer from './venues/VenueShowContainer';
 import BookingsList from './bookings/BookingsList';
@@ -13,8 +11,6 @@ const Main = (props) => {
       <Route exact path='/homes' component={ VenuesContainer } />
       <Route exact path='/homes/:id' component={ VenueShowContainer } />
       <ProtectedRoute exact path='/reservations' component={ BookingsList } />
-      <AuthRoute exact path='/signup' component={ Signup } />
-      <AuthRoute exact path='/login' component={ Login } />
     </section>
   );
 };
