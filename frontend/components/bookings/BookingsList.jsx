34a92a32e49cd4f68c5b27f3e9234c2dffed6d5a
andy yee <andy.sksea@gmail.com>
2017-09-24 00:42:43 -0400

    Add content to booking list

--- frontend/components/bookings/BookingsList.jsx
+++ frontend/components/bookings/BookingsList.jsx
@@ -31,17 +31,17 @@ class BookingsList extends React.Component {
     const { bookings, venues } = this.props;
     return (
       <div className='bl-wrapper'>
-        <div className='bookings-list'>
-          <BookingsListTitle/>
-          <BookingListTable>
-            <BookingsListTH/>
-            { Object.values(bookings).map( booking => {
-                return <Booking key={ booking.id }
-                  {...booking}
-                  venue={ venues[booking.venue_id] } />;
-            })}
-          </BookingListTable>
-        </div>
+          <div className='bookings-list'>
+            <BookingsListTitle/>
+            <BookingListTable>
+              <BookingsListTH/>
+              { Object.values(bookings).map( booking => {
+                  return <Booking key={ booking.id }
+                    {...booking}
+                    venue={ venues[booking.venue_id] } />;
+              })}
+            </BookingListTable>
+          </div>
       </div>
     );
   }
@@ -61,10 +61,10 @@ function BookingsListTitle(props) {
 function BookingsListTH() {
   return (
     <div className='bl-th'>
-      <div>Status</div>
-      <div>Dates and Location</div>
-      <div>Host</div>
-      <div>Details</div>
+      <div className='status'>Status</div>
+      <div className='dates-location'>Dates and Location</div>
+      <div className='host'>Host</div>
+      <div className='details'>Details</div>
     </div>
   );
 }
@@ -81,22 +81,33 @@ function Booking(props) {
   const {
     check_in: checkIn,
     check_out: checkOut,
-    venue_id: venueId,
+    venue,
   } = props;
   return (
     <div className='booking'>
-      <div className='b-status'></div>     
-      <BookingDates checkIn={ checkIn } checkOut={ checkOut } />
-      <div className='b-guest'></div>     
-      <div className='b-details'></div>     
+      <div className='status'>Accepted</div>     
+      <BookingDatesLocation 
+        checkIn={ checkIn } checkOut={ checkOut }  
+        venue={ venue } />
+      <div className='guest'></div>     
+      <div className='details'></div>     
     </div>
   );
 }
 
-function BookingDates({ checkIn, checkOut }) {
-  return (
-    <div className='b-dates'>
-      { readableDateRange(checkIn, checkOut) }
-    </div>
-  );
+function BookingDatesLocation({ checkIn, checkOut, venue }) {
+  if (venue) {
+    const { street, city, state, postal_code } = venue;
+    return (
+      <div className='dates-location'>
+        <div>{ readableDateRange(checkIn, checkOut) }</div>
+        <div>{ venue.name }</div>
+        <div>{ `${street}, ${city}, ${state} ${postal_code}` }</div>
+      </div>
+    );
+  } else {
+    return (
+      <div></div>
+    );
+  }
 }
